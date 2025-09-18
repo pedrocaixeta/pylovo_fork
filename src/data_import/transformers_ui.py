3849d8b19f5545d1f0b0e1c6d1b1cfe026228d94
bereba <beneharo.reveron-baecker@tum.de>
2025-09-18 17:53:05 +0200

    Adjust GUI title

--- src/data_import/transformers_ui.py
+++ src/data_import/transformers_ui.py
@@ -354,7 +354,7 @@ class TransformerMapUI:
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Transformer Map UI - v2.0</title>
+        <title>Transformer Map UI</title>
         <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate">
         <meta http-equiv="Pragma" content="no-cache">
         <meta http-equiv="Expires" content="0">
